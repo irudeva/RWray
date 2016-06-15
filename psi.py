ae263fd9011270d77636016d22cec9fdacec2228
irudeva <irinarudeva@gmail.com>
2016-06-15 14:09:59 +1000

    Add files via upload

---
+++ psi.py
@@ -0,0 +1,54 @@
+#import cartopy.crs as ccrs
+#from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+#from cartopy.util import add_cyclic_point
+#import matplotlib as mpl
+#mpl.rcParams['mathtext.default'] = 'regular'
+#import matplotlib.pyplot as plt
+from netCDF4 import Dataset
+import numpy as np
+
+from windspharm.standard import VectorWind
+from windspharm.tools import prep_data, recover_data, order_latdim
+
+
+# Read zonal and meridional wind components from file using the netCDF4
+# module. The components are defined on pressure levels and are in separate
+# files.
+nc = Dataset('wnd300.mnth.erain.nc', 'r')
+uwnd = nc.variables['u'][:]
+vwnd = nc.variables['v'][:]
+lons = nc.variables['longitude'][:]
+lats = nc.variables['latitude'][:]
+nc.close()
+#ncv = Dataset('wnd.mnth.eraint.nc), 'r')
+#vwnd = ncv.variables['vwnd'][:]
+#ncv.close()
+#print uwnd.shape
+print uwnd(1,1,1)
+
+# The standard interface requires that latitude and longitude be the leading
+# dimensions of the input wind components, and that wind components must be
+# either 2D or 3D arrays. The data read in is 3D and has latitude and
+# longitude as the last dimensions. The bundled tools can make the process of
+# re-shaping the data a lot easier to manage.
+uwnd, uwnd_info = prep_data(uwnd, 'tyx')
+vwnd, vwnd_info = prep_data(vwnd, 'tyx')
+#print uwnd.shape
+
+# It is also required that the latitude dimension is north-to-south. Again the
+# bundled tools make this easy.
+lats, uwnd, vwnd = order_latdim(lats, uwnd, vwnd)
+
+# Create a VectorWind instance to handle the computation of streamfunction and
+# velocity potential.
+w = VectorWind(uwnd, vwnd)
+
+# Compute the streamfunction and velocity potential. Also use the bundled
+# tools to re-shape the outputs to the 4D shape of the wind components as they
+# were read off files.
+sf, vp = w.sfvp()
+sf = recover_data(sf, uwnd_info)
+vp = recover_data(vp, uwnd_info)
+
+
+print sf.shape
