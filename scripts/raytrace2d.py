5a9f73ad5d4e587f9fc2c7713b13c9878852189d
irudeva <irinarudeva@gmail.com>
2016-07-05 14:30:32 +1000

    add raytrace2d.py

---
+++ scripts/raytrace2d.py
@@ -0,0 +1,103 @@
+from netCDF4 import Dataset
+import numpy as np
+import datetime as datetime  # Python standard library datetime  module
+
+print "Calculating 2d ray paths"
+
+# Parameters
+
+pi = np.pi
+dtr=pi/180
+rtd=180/pi
+radius=6.371e6 #radius of sphere having same volume as Earth (m)
+e_omega=7.292e-5 #rotation rate of Earth (rad/s)
+
+
+day=24*60*60 #in seconds
+mins = 60
+Periods = np.array([float('inf'), 50, 20 ])*day
+
+freq = 2*pi/Periods
+nfreq=freq.size
+dt = 60 * mins   #time increment
+int_time=10*day   #integration time
+Nsteps = int_time/dt
+
+k_wavenumbers=np.array([1, 2, 3, 4, 5, 6]) #  initial k wave number:
+
+lon0 = np.array([0])
+lat0 = np.array([30])
+
+print '---Parameters---------'
+print "Wave periods: ", Periods/day, " days"
+print "Wave numbers: ", k_wavenumbers
+print "Periods: "
+print "integration time ", int_time/day, " days"
+print "time step ", dt/mins, " min"
+print "Nsteps = ",Nsteps
+print "Starting points: lon ",lon0,"E lat ",lat0,"N"
+print '--------------------'
+
+# Read data
+
+fw = "../data/wnd300.mnth.erain.nc"
+fsf = "../data/sf300.mnth.erain.nc"
+
+dimnam=('longitude','latitude','time')
+varnam=['longitude','latitude','time','u','v']
+
+print "Reading wind from", fw
+nc = Dataset(fw, 'r')
+v=0
+for var in varnam:
+    if nc.variables[varnam[v]].name != var:
+        print "Variables don't agree", var, nc.variables[varnam[v]].name, v
+        exit()
+    v += 1
+
+lons = nc.variables[varnam[0]][:]
+lats = nc.variables[varnam[1]][:]
+time = nc.variables[varnam[2]][:]
+uwnd = nc.variables[varnam[3]][:]
+vwnd = nc.variables[varnam[4]][:]
+
+print "Reading streamfunction from", fsf
+nc1 = Dataset(fsf, 'r')
+v=0
+for var in varnam[0:2]:
+    if nc1.variables[varnam[v]].name != var:
+        print "ERROR reading ",fsf,":"
+        print "Variables don't agree", var,"!=",nc.variables[varnam[v]].name
+        exit()
+    v += 1
+lons1 = nc1.variables[varnam[0]][:]
+lats1 = nc1.variables[varnam[1]][:]
+time1 = nc1.variables[varnam[2]][:]
+sf = nc.variables[varnam[3]][:]
+if (lons1!=lons).any():
+    print "ERROR wind.lons != streamfunction.lons"
+    exit()
+if (lats1!=lats).any():
+    print "ERROR wind.lats != streamfunction.lats"
+    exit()
+if (lons1!=lons).any():
+    print "ERROR wind.time != streamfunction.time"
+    exit()
+
+#  Time
+dt_time = [datetime.date(1900, 1, 1) + datetime.timedelta(hours=int(t))\
+           for t in time]
+
+nt=[-1 for i in range(time.size)]
+i =0
+for yr in range(1980,1982) :
+    for m in [12, 1, 2] :
+        yr1 = yr
+        if m == 12:
+            yr1 = yr-1
+        for t in dt_time :
+            if t == datetime.date(yr1,m,1):
+                print 'selected time: ', t
+                ind = dt_time.index(t)
+                nt[i] = ind
+                i += 1
